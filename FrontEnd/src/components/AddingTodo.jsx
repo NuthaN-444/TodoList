fb286cfa2e00b87f1a812c62d1c3540985401bf1
NuthaN-444 <nuthanprasadkg@gmail.com>
2026-02-04 13:54:30 +0530

    changes in frontend addingTodo.js small changed

--- FrontEnd/src/components/AddingTodo.jsx
+++ FrontEnd/src/components/AddingTodo.jsx
@@ -43,6 +43,7 @@ const AddingTodo = () => {
       <button className='Log-out' onClick={() => {
         setIsUserLogin(false)
         setUserEmail(null);
+        setTodoList(null)
         }}>Log out</button>
       <div className='Adding-Todo-List-Div'>
       <input type="text" placeholder='Write Todo' value={todoTitle} onChange={(e) => setTodoTitle(e.target.value)}/>
